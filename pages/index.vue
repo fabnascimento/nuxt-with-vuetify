b4a1153a8272404e635346ee0753a86b6fa7242e
Fabricio Nascimento <f.f.nascimento9@gmail.com>
2020-12-03 23:44:37 -0300

    add list component

--- pages/index.vue
+++ pages/index.vue
@@ -5,12 +5,37 @@
       <v-toolbar-title v-text="title" />
       <v-spacer />
     </v-app-bar>
-    <SitesSkeleton :active="sites.length < 1" />
-    <div v-for="site in sites" :key="site.id">
-      <NuxtLink :to="`/site/${site.id}`">
-        <span>{{ site.title }}</span>
-      </NuxtLink>
+    <div>
+      <!-- Toolbar -->
     </div>
+    <SitesSkeleton :active="sites.length < 1" />
+    <v-list three-line>
+      <template v-for="site in sites">
+        <NuxtLink :key="site.id" :to="`/site/${site.id}`">
+          <v-list-item :key="site.id">
+            <v-list-item-avatar>
+              <v-img :src="site.images[0]"></v-img>
+            </v-list-item-avatar>
+            <v-list-item-content>
+              <v-list-item-title>
+                <span>{{ site.title }}</span>
+              </v-list-item-title>
+              <v-list-item-subtitle>
+                {{
+                  `${site.address.street}, ${site.address.city} - ${site.address.state}`
+                }}
+              </v-list-item-subtitle>
+              <v-list-item-subtitle>
+                {{
+                  `${site.contacts.main.firstName} ${site.contacts.main.lastName}`
+                }}
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-divider />
+        </NuxtLink>
+      </template>
+    </v-list>
   </div>
 </template>
 <script>
